32a09aee72db4acf1a2a2325b39d35be1d89f1a1
rosshemsley <rlhemsley@gmail.com>
2018-07-20 22:18:53 +0100

    init

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+echo -e "🚢 Pushing to GitHub..."
+pushd rosshemsley.co.uk
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. giving up"
+    exit 1;
+fi
+
+rm -rf public
+mkdir public
+git worktree prune
+popd
+rm -rf .git/worktrees/public/
+git worktree add rosshemsley.co.uk/public gh-pages
+pushd rosshemsley.co.uk
+rm -rf public/*
+
+hugo
+
+cd public && git add --all && git commit -m "Publishing to gh-pages"
+git push
